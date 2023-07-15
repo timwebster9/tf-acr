ffcb06c2b61952114078e81dd6bcbeb2f9d66b99
timw2 <tim.webster9@protonmail.com>
2023-07-15 11:34:47 +0100

    update

--- main.tf
+++ main.tf
@@ -54,6 +54,34 @@ resource "azurerm_container_registry_task" "lemmy_nginx" {
   }
 }
 
+resource "azurerm_container_registry_task" "lemmy_caddy" {
+  name                  = "build-lemmy-caddy"
+  container_registry_id = azurerm_container_registry.acr.id
+  platform {
+    os = "Linux"
+  }
+
+  docker_step {
+    dockerfile_path      = "Dockerfile"
+    context_path         = "https://github.com/timwebster9/caddy-lemmy#main"
+    context_access_token = var.pat_token
+    image_names          = ["lemmy-caddy:{{.Run.ID}}"]
+  }
+
+  source_trigger {
+    name           = "build-lemmy-caddy"
+    events         = ["commit"]
+    repository_url = "https://github.com/timwebster9/caddy-lemmy.git"
+    source_type    = "Github"
+    branch         = "main"
+
+    authentication {
+      token = var.pat_token
+      token_type = "PAT"
+    }
+  }
+}
+
 resource "azurerm_container_registry_task" "lemmy" {
   name                  = "build-lemmy"
   container_registry_id = azurerm_container_registry.acr.id
